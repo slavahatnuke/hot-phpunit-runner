43af05114581e0add0016f94922d5036f67253c8
Brian Webb <bwebb@indatus.com>
2014-01-18 13:32:30 -0500

    fix terminal-notifier command to work for osx-mavericks

--- src/Hot/Notify/Notify.php
+++ src/Hot/Notify/Notify.php
@@ -9,7 +9,7 @@ class Notify
         $x_message = $this->esc($message);
 
         if ($this->execute('which terminal-notifier')) {
-            $this->execute("terminal-notifier -title '{$x_title}' -message '{$x_message}'");
+            $this->execute("terminal-notifier -title '{$x_title}' -message '{$x_message}' -sender com.apple.Terminal");
         } else if ($this->execute('which notify-send')) {
             $this->execute("notify-send -t 2000 '{$x_title}' '$x_message'");
         } else {
